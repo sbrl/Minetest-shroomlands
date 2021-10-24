e18626280536f290b16f3ee1ace2e3f7ad98773b
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-10-24 20:29:42 +0100

    write initial init.lua file

---
+++ init.lua
@@ -0,0 +1,16 @@
+--- silverell_plants
+-- @module shroomlands
+-- @release 0.1
+-- @copyright 2018 Starbeamrainbowlabs
+-- @license Mozilla Public License, 2.0 (MPL-2.0)
+-- @author Starbeamrainbowlabs
+
+shroomlands = {  }
+
+local modpath = minetest.get_modpath("silverbell_plants")
+
+
+-- TODO: Load mod files here
+
+
+print("[shroomlands] Loaded and ready")
